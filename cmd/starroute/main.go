2d32a0572a767bf8365b15617fefc922f48fdef1
udhos <udhos>
2025-11-29 01:34:44 -0300

    Add sprite.

--- cmd/starroute/main.go
+++ cmd/starroute/main.go
@@ -2,19 +2,86 @@
 package main
 
 import (
+	"bytes"
+	"image"
 	"log"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
 )
 
+type sprite struct {
+	x, y          float64
+	width, height int
+	angle         float64
+}
+
+const maxAngle = 100 // custom number of angles in the circle
+
+func (s *sprite) update() {
+	// move sprint etc
+	s.angle = math.Mod(s.angle+1, maxAngle)
+}
+
 // game implements ebiten.Game interface.
-type game struct{}
+type game struct {
+	op          ebiten.DrawImageOptions
+	sprites     []*sprite
+	ebitenImage *ebiten.Image
+}
+
+func newGame() *game {
+
+	//
+	// Load an image from the embedded image data.
+	//
+
+	// Decode an image from the image file's byte slice.
+	img, _, err := image.Decode(bytes.NewReader(images.Ebiten_png))
+	if err != nil {
+		log.Fatalf("newGame: %v", err)
+	}
+	origEbitenImage := ebiten.NewImageFromImage(img)
+
+	s := origEbitenImage.Bounds().Size()
+	ebitenImage := ebiten.NewImage(s.X, s.Y)
+
+	op := &ebiten.DrawImageOptions{}
+	op.ColorScale.ScaleAlpha(0.5)
+	ebitenImage.DrawImage(origEbitenImage, op)
+
+	g := &game{ebitenImage: ebitenImage}
+
+	//
+	// Add one sprite to the game.
+	//
+
+	w, h := ebitenImage.Bounds().Dx(), ebitenImage.Bounds().Dy()
+	x, y := 50, 50
+
+	spr := sprite{
+		x:      float64(x),
+		y:      float64(y),
+		width:  w,
+		height: h,
+	}
+	g.sprites = append(g.sprites, &spr)
+
+	return g
+}
 
 // Update is called every tick. Tick is a time unit for logical updating.
 // The default value is 1/60 [s], then Update is called 60 times per second by
 // default (i.e. an Ebitengine game works in 60 ticks-per-second).
 func (g *game) Update() error {
+
+	// Update all sprites.
+	for _, spr := range g.sprites {
+		spr.update()
+	}
+
 	return nil
 }
 
@@ -29,6 +96,24 @@ func (g *game) Update() error {
 // rendering. The window shows the final state of screen every frame.
 func (g *game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")
+
+	// Draw each sprite.
+	// DrawImage can be called many many times, but in the implementation,
+	// the actual draw call to GPU is very few since these calls satisfy
+	// some conditions e.g. all the rendering sources and targets are same.
+	// For more detail, see:
+	// https://pkg.go.dev/github.com/hajimehoshi/ebiten/v2#Image.DrawImage
+	w, h := g.ebitenImage.Bounds().Dx(), g.ebitenImage.Bounds().Dy()
+	for i := 0; i < len(g.sprites); i++ {
+		s := g.sprites[i]
+		g.op.GeoM.Reset()
+		g.op.GeoM.Translate(-float64(w)/2, -float64(h)/2)
+		g.op.GeoM.Rotate(2 * math.Pi * float64(s.angle) / maxAngle)
+		g.op.GeoM.Translate(float64(w)/2, float64(h)/2)
+		g.op.GeoM.Translate(float64(s.x), float64(s.y))
+		screen.DrawImage(g.ebitenImage, &g.op)
+	}
+
 }
 
 // Layout accepts an outside size, which is a window size on desktop, and
@@ -43,7 +128,11 @@ func (g *game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&game{}); err != nil {
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+	g := newGame()
+
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
